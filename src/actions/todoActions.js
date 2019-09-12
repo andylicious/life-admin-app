b7a24dff9770162a23b20bdb0f87bb5fc16de5bb
Andreas Mann <andreasmann94@gmail.com>
2019-09-12 17:40:43 +0200

    Checking box now updates

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -10,17 +10,6 @@ export const addTodo = data => dispatch => {
     .catch(err => dispatch({ type: ADD_TODO_FAILURE, payload: err }));
 };
 
-export const CHECK_TODO = 'CHECK_TODO';
-export const CHECK_TODO_FAILURE = 'CHECK_TODO_FAILURE';
-
-export const checkTodo = data => dispatch => {
-  console.log(data);
-  axios
-    .put(`http://localhost:5000/api/v1/todos/${data._id}`, data)
-    .then(res => dispatch({ type: CHECK_TODO, payload: res.data }))
-    .catch(err => dispatch({ type: CHECK_TODO_FAILURE, payload: err }));
-};
-
 export const GET_TODOS = 'GET_TODOS';
 export const GET_TODOS_FAILURE = 'GET_TODOS_FAILURE';
 
@@ -29,4 +18,16 @@ export const getTodos = () => dispatch => {
     .get('http://localhost:5000/api/v1/todos/sorted')
     .then(res => dispatch({ type: GET_TODOS, payload: res.data }))
     .catch(err => dispatch({ type: GET_TODOS_FAILURE, payload: err }));
+};
+
+export const CHECK_TODO = 'CHECK_TODO';
+export const CHECK_TODO_FAILURE = 'CHECK_TODO_FAILURE';
+
+export const checkTodo = data => dispatch => {
+  console.log(data);
+  axios
+    .put(`http://localhost:5000/api/v1/todos/${data._id}`, data)
+    .then(res => dispatch({ type: CHECK_TODO, payload: res.data }))
+    .catch(err => dispatch({ type: CHECK_TODO_FAILURE, payload: err }))
+    .then(() => dispatch(getTodos()));
 };
